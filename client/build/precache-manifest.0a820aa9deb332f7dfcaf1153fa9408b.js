3dbd6ec5ef807d3315d4397caccd0f671d057ac7
franklin,petra <petrafranklin.gmail.com>
2020-10-30 11:32:20 -0700

    initial commit

---
+++ client/build/precache-manifest.0a820aa9deb332f7dfcaf1153fa9408b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be086c135a0af8ed8c66c4dbd41685ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aca3a6d149b8d793f04",
+    "url": "/static/css/main.2fe26253.chunk.css"
+  },
+  {
+    "revision": "27c0718123d74d728bc4",
+    "url": "/static/js/2.52646ee3.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.52646ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aca3a6d149b8d793f04",
+    "url": "/static/js/main.bd44ca78.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9e9c7dd26851595514cce30866ec2478",
+    "url": "/static/media/xIcon.9e9c7dd2.png"
+  }
+]);
