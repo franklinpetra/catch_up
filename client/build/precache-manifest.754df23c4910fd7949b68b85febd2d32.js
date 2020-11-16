bf2f1e74b2e264e4eb454096386a1a12a052ecf4
franklin,petra <petrafranklin.gmail.com>
2020-11-15 16:38:07 -0800

    build

---
+++ client/build/precache-manifest.754df23c4910fd7949b68b85febd2d32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ae8a8c17c61f14a9ab9be7b917391c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f1ea69d2aefa26511fd",
+    "url": "/static/css/main.2fe26253.chunk.css"
+  },
+  {
+    "revision": "27c0718123d74d728bc4",
+    "url": "/static/js/2.52646ee3.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.52646ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1ea69d2aefa26511fd",
+    "url": "/static/js/main.d86980ba.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9e9c7dd26851595514cce30866ec2478",
+    "url": "/static/media/xIcon.9e9c7dd2.png"
+  }
+]);
